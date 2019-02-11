1ad8fbf9e0dbcb2ffecfb832808e9f64e1f54e5f
_vasilie <vasilije.milenkovic@diwanee.com>
2019-02-11 11:41:40 +0100

    font, inline svg

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,8 +7,14 @@
           <a href='#'><li class="nav__item">Beaty</li></a>
           <a href='#'><li class="nav__item">Sport</li></a>
         </ul>
-        
-        <img src="/src/images/search.svg" alt="" class='search-ico'>
+        <svg class='search-ico' xmlns="http://www.w3.org/2000/svg" id="search" viewBox="0 0 11.855 11.855">
+            <defs>
+                <style>
+                    .cls-1{fill:#fff;fill-rule:evenodd}
+                </style>
+            </defs>
+            <path id="Path_21" d="M11.565 10.164L8.989 7.588c-.014-.014-.03-.023-.044-.036a4.876 4.876 0 1 0-1.393 1.393c.013.014.022.03.036.044l2.575 2.575a.991.991 0 0 0 1.4-1.4zM4.874 8.058a3.184 3.184 0 1 1 3.184-3.184 3.184 3.184 0 0 1-3.184 3.184z" class="cls-1" data-name="Path 21"/>
+        </svg>
         <ul class="nav nav--right">
           <a href='#'><li class="nav__item">category</li></a>
         </ul>
